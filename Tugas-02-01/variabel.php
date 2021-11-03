a7ee28e60e05d412573f664c7607072c1c7f11a9
Ridha30 <ridha19@mhs.akba.ac.id>
2021-11-03 11:25:21 +0800

    Commit Pertama

---
+++ Tugas-02-01/variabel.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * ini adalah nilai awal variabel
+ */
+ $nama = "Nurkiswani";
+ $usia = 22;
+ echo "<p>Hai, perkenalkan nama saya adalah ".$nama.", dan saya
+ berusia ".$usia." tahun<p>";
+ 
+  ?>
